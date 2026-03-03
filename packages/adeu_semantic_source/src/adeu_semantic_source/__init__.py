2fcb33dfcbbb101a8ceb28cdd7eb7fc0eba4d9e8
Venus <45287395+andreidita22@users.noreply.github.com>
2026-03-03 17:57:48 +0200

    contracts: add V32-B semantic-source grammar/parser/normalizer MVP

---
+++ packages/adeu_semantic_source/src/adeu_semantic_source/__init__.py
@@ -0,0 +1,15 @@
+from .compile import (
+    SEMANTIC_SOURCE_COLLECTION_SCHEMA,
+    SEMANTIC_SOURCE_DIAGNOSTICS_SCHEMA,
+    CompileResult,
+    compile_semantic_source,
+)
+
+__all__ = [
+    "SEMANTIC_SOURCE_COLLECTION_SCHEMA",
+    "SEMANTIC_SOURCE_DIAGNOSTICS_SCHEMA",
+    "CompileResult",
+    "compile_semantic_source",
+]
+
+__version__ = "0.0.0"
